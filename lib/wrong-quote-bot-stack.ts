30fc785b3e98bc407c5b996b241ca0634537ffb8
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-01-07 13:15:31 -0500

    set up lambda + cronjob stack

--- lib/wrong-quote-bot-stack.ts
+++ lib/wrong-quote-bot-stack.ts
@@ -1,18 +1,30 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as targets from "@aws-cdk/aws-events-targets";
+import * as events from "@aws-cdk/aws-events";
+import * as cdk from "@aws-cdk/core";
+require("dotenv").config();
 
 export class WrongQuoteBotStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'WrongQuoteBotQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const { TWITTER_SECRET_ARN } = process.env;
+
+    const lambdaFunction = new lambda.Function(this, "CryptoTradingBot", {
+      code: new lambda.AssetCode("lambda", {
+        exclude: ["*.ts", "*.d.ts"],
+      }),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      functionName: "WrongQuoteBot",
+      environment: {
+        TWITTER_SECRET_ARN: TWITTER_SECRET_ARN || "",
+      },
     });
 
-    const topic = new sns.Topic(this, 'WrongQuoteBotTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const eventRule = new events.Rule(this, "quoteBotCronJob", {
+      schedule: events.Schedule.cron({ minute: "0", hour: "15" }),
+    });
+    eventRule.addTarget(new targets.LambdaFunction(lambdaFunction));
   }
 }
