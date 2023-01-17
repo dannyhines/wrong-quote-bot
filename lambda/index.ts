3c45af94245f2d5672395138041a5588cfc01b0f
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-01-16 22:43:22 -0500

    cleanup files in /lambda

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,6 +1,6 @@
-import { getRandomQuote } from "./quote";
-import { sendTweet } from "./twitter";
-import { randomStringFromFile } from "./helpers";
+import { getRandomQuote } from "./src/quote";
+import { sendTweet } from "./src/twitter";
+import { randomStringFromFile } from "./src/helpers";
 
 exports.handler = async function (event: any, _context: any) {
   try {
