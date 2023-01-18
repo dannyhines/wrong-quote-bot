bdaf2452bfa7878f60092e32127b38ac6fcf78f0
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-01-17 22:13:49 -0500

    print more quotes for testing

--- lambda/src/print.ts
+++ lambda/src/print.ts
@@ -1,8 +1,27 @@
 import { randomStringFromFile } from "./helpers";
-import { getRandomQuote } from "./quote";
+import {
+  getLotrQuote,
+  getMovieQuote,
+  getQuotableQuote,
+  getRandomQuote,
+  getRapQuote,
+  getSportsQuote,
+  getTheOfficeQuote,
+  getTrumpQuote,
+} from "./quote";
 
+// Called for testing purposes
 (async () => {
   const quote = await getRandomQuote();
   const name = randomStringFromFile("famousPeople.txt");
+  console.log("--------------------------------------");
   console.log(`"${quote}"\n-${name}`);
+  console.log("--------------------------------------");
+  console.log(`LOTR:\t"${getLotrQuote()}"`);
+  console.log(`SPORTS:\t"${getSportsQuote()}"`);
+  console.log(`TRUMP:\t"${await getTrumpQuote()}"`);
+  console.log(`OFFICE:\t"${getTheOfficeQuote()}"`);
+  console.log(`QUOTE:\t"${await getQuotableQuote()}"`);
+  console.log(`MOVIE:\t"${getMovieQuote()}"`);
+  console.log(`RAP:\t"${getRapQuote()}"`);
 })();
